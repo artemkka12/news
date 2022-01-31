9f8939c82ffbe0059ecbd1a265475ff3a43397cf
Artem <artemkka2280@gmail.com>
2022-01-31 17:59:04 +0200

    added tracking user ip

--- news/views.py
+++ news/views.py
@@ -83,15 +83,15 @@ class NewsByCategory(ListView):
 
 class ViewNews(DetailView):
     model = News
-    users_ip = []
 
     def get_object(self, queryset=None):
         obj = get_object_or_404(News, pk=self.kwargs.get('pk'))
+        obj.users_ip = []
         ip = get_client_ip(request=self.request)
-        if ip not in self.users_ip:
+        if ip not in obj.users_ip:
             obj.views += 1
             obj.save()
-            self.users_ip.append(ip)
+            obj.users_ip.append(ip)
         return obj
 
 
