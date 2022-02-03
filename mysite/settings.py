216f9bfc870557d5dfae45ad842fc01c0f4309bf
Artem <artemkka2280@gmail.com>
2022-02-03 17:21:55 +0200

    photo

--- mysite/settings.py
+++ mysite/settings.py
@@ -132,5 +132,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
